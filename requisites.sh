5becd9f5e5df27309d3e1c1c1b13d25e2ce31ca4
Royce Richmond <miles-militis@hotmail.com>
2020-05-24 18:54:02 -0500

    Add files via upload
    sh file updated works fine with the jupyert notebooks and numpy correction

--- requisites.sh
+++ requisites.sh
@@ -1,10 +1,20 @@
-sudo apt-get update
-sudo -H apt install python3-pip python3-pil
-sudo apt-get install nano -y
 sudo usermod -aG i2c $USER
-
-
-pip3 install Adafruit-SSD1306
+sudo apt-get update
+sudo apt-get install nano -y
+sudo -H apt install python3-pip python3-pil
+sudo pip3 install cython
+sudo pip3 install --upgrade numpy 
+echo "numpy and python 3 pip installed"
+##optional libraries for the jupyter notebook
+sudo apt install nodejs npm
+sudo pip3 install jupyter jupyterlab
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter lab --generate-config
+jupyter notebook password
+##libraries for the peripherals
+sudo pip3 install Adafruit-SSD1306
+echo "Oled library installed"
 sudo pip3 install pyserial adafruit-circuitpython-gps
 sudo pip3 install --upgrade adafruit_blinka
 sudo apt autoremove
+echo "GPS library installed"
